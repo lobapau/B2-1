856a18b09c1e6ec92a621656448cc07868f6538d
wilsonmorales2002 <113655942+wilsonmorales2002@users.noreply.github.com>
2022-10-24 19:56:10 -0500

    Add files via upload

---
+++ cc+/calcular la edad.cpp
@@ -0,0 +1,17 @@
+//calcular saldo
+#include <iostream>
+cout << "Ingresa su saldo: ";
+int main()
+{
+    int sueldo;  //sueldo
+    int luz; //gasto
+    int agua; //gasto
+    int serviciosbasicos; //
+    int internet; //gasto
+    
+agua=sueldo*0.2
+luz=sueldo*0.2
+serviciosbasicos=sueldo*0.4
+internet=0.1
+ 
+cout << "Ingresa el valor de peso en kg: ";
